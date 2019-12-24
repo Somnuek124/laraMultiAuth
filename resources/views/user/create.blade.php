3ca7a56c727ec9bea76d01d32ec7dbec4b109773
Somnuek124 <somnuek124@outlook.com>
2019-12-24 09:44:02 +0700

    last2

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
+    <div class="row">
+        <div class="col-md-8">
+            <div >
+                <div >{{ __('Register') }}</div>
+
+                <div >
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -72,5 +72,5 @@
             </div>
         </div>
     </div>
-</div>
+
 @endsection
